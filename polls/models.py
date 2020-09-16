5969795b0ba1a57737788e07109dc062979d51e1
antony-raul <raulzin.raul@gmail.com>
2020-09-16 09:37:04 -0300

    parte2

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,23 @@
+from datetime import timedelta
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField('Texto da questão',max_length=200,help_text='informe o texto da questão')
+    pub_date = models.DateTimeField('Data de publição',)
+
+    def was_published_recently(self):
+        ontem = timezone.now() - timedelta(days=1)
+        return self.pub_date >= ontem
+
+
+    def __str__(self):
+        return self.question_text
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete= models.CASCADE, verbose_name='questão' )
+    choice_text = models.CharField('texto da escolha',max_length=200)
+    votes = models.IntegerField('Nº de votos',default=0)
+
+    def __str__(self):
+        return self.choice_text
