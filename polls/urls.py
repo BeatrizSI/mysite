beed79290c7c5b9c34bf5b6dc5fcff75748e3124
antony-raul <55926088+antony-raul@users.noreply.github.com>
2020-09-25 15:21:44 -0300

    parte3

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>',views.detail, name='detail'),
     path('sobre', views.sobre, name='sobre'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,30 @@
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 
-def index(request):
-    print('Minha primeira view!')
-    return HttpResponse('Hello, world!')
+from polls.models import Question
+
+
+
+def index(request, template_name="polls/index.html"):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+
+    return render(request,template_name , context )
+
+def detail(request, question_id, template_name='polls/detail.html'):
+    question = get_object_or_404(Question,id=question_id)
+    
+
+    context = {
+        "question": question,
+    }
+
+    return render(request,template_name , context )
+
+
 
 def sobre(request):
     print('view - sobre')
